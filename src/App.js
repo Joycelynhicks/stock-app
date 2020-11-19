14b6022f0b2b1217520fa7e54f0eee9605ef4385
Joy Hicks <joyhicks@Joys-MacBook-Pro.local>
2020-11-18 21:01:42 -0700

    Updated Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li className="Nav">
-            Stock Trader
-          </li>
-          <li className='List'>
-            Welcome, User
-          </li>
-          <li className='List'>
-            Logout
-          </li>
-        </ul>
-      </nav>
+      <Navbar>
+        <Navbar.Brand href="#home">Stock App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+          <Navbar.Text>
+          <a href="#logout">Logout</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
       <header className="App-header">
       </header>
     </div>
