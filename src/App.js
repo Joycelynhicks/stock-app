1ec0122b7781c08d88c2f84e24099a3e0b9588ea
Joy Hicks <joyhicks@Joys-MacBook-Pro.local>
2020-11-20 09:17:57 -0700

    Added Grid file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navmain from './Components/Navmain'; 
+import Grid from './Components/Grid';
 
 function App() {
   return (
     <div className="App">
      
       <Navmain/>
+      <Grid/>
     </div>
   );
 }
