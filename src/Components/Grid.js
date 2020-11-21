0e1c4a530f6fb84f6357bcc4a85327d0ce877490
Joy Hicks <joyhicks@Joys-MacBook-Pro.local>
2020-11-20 19:09:52 -0700

    Added containers

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,7 +1,19 @@
 import React from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
-
+function Grid() {
+    return (
+      <div className="Grid">
+        <Container>
+            <Row className='Row'>
+                <Col className='columns'>1 of 2</Col>
+                <Col className='columns'>2 of 2</Col>
+            </Row>
+        </Container>
+      </div>
+    );
+  }
 
 
 
