d262b739866c420260416d70bd8e4f6506f3a927
Joy Hicks <joyhicks@Joys-MacBook-Pro.local>
2020-11-23 21:29:06 -0700

    Set state for cash available

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -5,9 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Counter() {
 
     const [count, setCount] = useState(0);
+    const [cash, setCash] = useState(100000);
     
     return (
       <div className="Counter">
+
+          <h1>Cash Available : {cash} </h1>
        
       </div>
     );
